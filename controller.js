65cf6e67422e79da6023611a46ad9a8c2f83f76e
Ignatius Timothy Manullang <13517044@std.stei.itb.ac.id>
2019-11-23 16:39:10 +0700

    Feature integration with engima

--- controller.js
+++ controller.js
@@ -2,10 +2,10 @@ const response = require('./res');
 const connection = require('./conn');
 
 exports.findAllUserTransaction = function (req, res) {
-  const { userId } = req.query;
+  const { userID } = req.query;
 
   connection.query('SELECT * FROM transactions where user_id = ?',
-    [userId],
+    [userID],
     (error, rows) => {
       if (error) {
         console.log(error);
@@ -17,28 +17,29 @@ exports.findAllUserTransaction = function (req, res) {
 
 exports.createTransaction = function (req, res) {
   const {
-    virtualAccount, movieId, schedule, chair, userId,
-  } = req.body;
-
+    virtualAccount, movieId, historyDate, historyTime, chair, userId,
+  } = req.query;
+  
   const currentTime = new Date();
   const createdAt = `${currentTime.getFullYear()}-${currentTime.getMonth() + 1}-${currentTime.getDate()} ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
 
-  connection.query('INSERT INTO transactions (virtual_account, movie_id, schedule, chair, created_at, user_id) values (?, ?, ?, ?, ?, ?)',
-    [virtualAccount, movieId, schedule, chair, createdAt, userId],
+  connection.query('INSERT INTO transactions (virtual_account, movie_id, historyDate, historyTime, chair, created_at, user_id) values (?, ?, ?, ?, ?, ?, ?)',
+    [virtualAccount, movieId, historyDate, historyTime, chair, createdAt, userId],
     (error, result) => {
       if (error) {
         console.log(error);
       } else {
+        //response.successPost(result.insertId, res);
         response.successPost(result.insertId, res);
       }
     });
 };
 
 exports.updateTransaction = function (req, res) {
-  const { id, status } = req.body;
+  const { transaction_id, status } = req.body;
 
-  connection.query('UPDATE transactions SET status = ? WHERE id = ?',
-    [status, id],
+  connection.query('UPDATE transactions SET status = ? WHERE transaction_id = ?',
+    [status, transaction_id],
     (errorUpdate) => {
       if (errorUpdate) {
         console.log(errorUpdate);
@@ -49,5 +50,5 @@ exports.updateTransaction = function (req, res) {
 };
 
 exports.index = function (req, res) {
-  response.success('Hello world!', res);
+  response.success('Hello worlds!', res);
 };
